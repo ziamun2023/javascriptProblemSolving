5d88c9fd70a8028c04e7c8d4e303a0dd8bbc4f8a
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-07-27 20:08:32 +0600

    task 2 completed

--- task-2.js
+++ task-2.js
@@ -1,9 +1,9 @@
 // Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
 
 const SumOfPositiveNum=(numArray)=>{
-    const allPositiveNum=numArray.filter((element)=>element%2===0)
+    const allPositiveNum=numArray.filter((element)=>element>0)
     const sumOfPostiveNum=allPositiveNum.reduce((a,b)=>a+b)
     console.log(sumOfPostiveNum)
 
 }
-SumOfPositiveNum([2,3,4,5,6,7,8])
+SumOfPositiveNum([2,3,4,5,6,0,-3,-20,-2,7,8])
